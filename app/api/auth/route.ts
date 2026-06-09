fcecd33a6ba4755a5e2d8895fb1795d7e19ea7c7
miyocare <miyocare@gmail.com>
2026-06-10 04:36:21 +0900

    ログインセッションを10年間に延長

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: NextRequest) {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 60 * 60 * 24 * 365, // 1年間
+      maxAge: 60 * 60 * 24 * 365 * 10, // 10年間
     })
     return res
   }
